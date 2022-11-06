ea5bb3f6efead946ba5e3dd1fb2601f713e9c3f6
Trina <trina.rundgren@telus.com>
2022-11-05 20:14:14 -0700

    commit updates

---
+++ perfect-shot.js
@@ -0,0 +1,28 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
+
+const finalPosition = (moves) >= {
+  let position = [0, 0]
+
+  for (let x of moves) {
+    if (x === "west") position[0] --
+    else if (x === "east") position[0] ++
+    else if (x === "north") position[0] ++
+    else postion[1] --
+  }
+  return position
+}
+
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+const finalPosition = (moves) => {
+  let position = [0, 0]
+
+  for (let i of moves){
+      if (i === "west")  position[0] --
+      else if (i === "east")  position[0] ++
+      else if (i === "north")  position[1] ++
+      else  position[1] --
+    }
+    return  position   
+}
+console.log(finalPosition(moves))
---
+++ voting.stations.js
@@ -0,0 +1,14 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+const chooseStations = function(stations) => {
+  let appropriateStations = [];
+  for (let station of stations) {
+    if((station[1] >= 20) && station[2] === "school" || station[2] === "community centre"){
+      appropriateStations.push(station[0]); }
+  }
+  return appropriateStations
+}
+console.log(chooseStations(stations))
